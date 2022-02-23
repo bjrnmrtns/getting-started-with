7da4b6ff60d3b05c8ea0b6b5692b2b4657ce1ea4
Bjorn Martens <bjorn@expeditious.nl>
2022-02-23 10:16:43 +0100

    finished typescript tutorial

--- typescript/src/app.ts
+++ typescript/src/app.ts
@@ -1,25 +1,7 @@
-class Invoice {
-    constructor(
-        public client: string,
-        public details: string,
-        public amount: number,
-    ){}
-
-    format() {
-        return `${this.client} owes $${this.amount} for ${this.details}`
-    }
-}
-
-const invoiceOne = new Invoice('mario', 'work on mario website', 250);
-const invoiceTwo = new Invoice('luigi', 'work on luigi website', 300);
-
-let invoices: Invoice[] = [];
-invoices.push(invoiceOne);
-invoices.push(invoiceTwo);
-
-invoices.forEach(inv => {
-    console.log(inv.client, inv.details, inv.amount, inv.format());
-})
+import {Invoice} from './classes/invoice.js'
+import { Payment } from './classes/payment.js';
+import { ListTemplate } from './classes/renderlisttemplate.js';
+import { HasFormatter } from './interfaces/hasformatter.js';
 
 const form = document.querySelector('.new-item-form') as HTMLFormElement;
 
@@ -28,8 +10,56 @@ const tofrom = document.querySelector('#tofrom') as HTMLInputElement;
 const details = document.querySelector('#details') as HTMLInputElement;
 const amount = document.querySelector('#amount') as HTMLInputElement;
 
+const ul = document.querySelector('ul');
+const list = new ListTemplate(ul!);
+
+let tuple: [string, string, number] = ['ken', 'beer bought', 343];
+new Invoice(...tuple);
+
 form.addEventListener('submit', (e: Event) => { 
     e.preventDefault();
-    console.log(type.value, tofrom.value, details.value, amount.valueAsNumber);
+
+    let doc: HasFormatter;
+    if(type.value === 'invoice') {
+        doc = new Invoice(tofrom.value, details.value, amount.valueAsNumber);
+    } else {
+        doc = new Payment(tofrom.value, details.value, amount.valueAsNumber);
+    }
+
+    list.render(doc, type.value, 'end');
 });
 
+enum ResourceType {
+    BOOK,
+    AUTHOR,
+    DIRECTOR,
+}
+
+
+const addUID = <T extends object>(obj: T) => {
+    let uid = Math.floor(Math.random() * 100);
+    return {...obj, uid};
+}
+
+let docOne = addUID({name: 'yoshi', age: 32});
+console.log(docOne);
+
+interface Resource<T> {
+    uid: number;
+    resourceType: ResourceType;
+    data: T;
+}
+
+const docThree: Resource<object> = {
+    uid: 1,
+    resourceType: ResourceType.DIRECTOR,
+    data: { name: 'shaun'},
+}
+
+const docFour: Resource<string[]> = {
+    uid: 2,
+    resourceType: ResourceType.AUTHOR,
+    data: ['bread', 'beer'],
+}
+
+console.log(docThree, docFour);
